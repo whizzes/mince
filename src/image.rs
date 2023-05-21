3ebf78571249a31680a9cbdf45f625310f722de7
Esteban Borai <estebanborai@gmail.com>
2023-05-21 01:11:30 -0400

    feat: resize images into a new `Mince` instance

--- src/image.rs
+++ src/image.rs
@@ -1,6 +1,6 @@
 use std::io::Cursor;
 
-use anyhow::Result;
+use image::imageops::FilterType;
 use image::io::Reader as ImageReader;
 use image::{DynamicImage, ImageFormat};
 use js_sys::Uint8Array;
@@ -8,16 +8,69 @@ use wasm_bindgen::prelude::*;
 use wasm_bindgen_futures::JsFuture;
 use web_sys::File;
 
-use crate::log;
+/// Supported image formats for `Mince`
+#[wasm_bindgen]
+#[derive(Clone, Copy, Debug, Default, PartialEq, Eq)]
+pub enum Format {
+    Jpeg,
+    Png,
+    Gif,
+    #[default]
+    Unsupported,
+}
+
+impl From<ImageFormat> for Format {
+    fn from(value: ImageFormat) -> Self {
+        match value {
+            ImageFormat::Jpeg => Format::Jpeg,
+            ImageFormat::Png => Format::Png,
+            ImageFormat::Gif => Format::Gif,
+            _ => Format::Unsupported,
+        }
+    }
+}
+
+/// Metadata for a `Mince` instance, which provides relevant details on the
+/// contained image instance
+#[wasm_bindgen]
+#[derive(Clone, Copy, Debug, Default, PartialEq, Eq)]
+pub struct Metadata {
+    pub width: u32,
+    pub height: u32,
+    pub format: Format,
+}
 
 #[wasm_bindgen]
+impl Metadata {
+    pub fn new(width: u32, height: u32, format: Format) -> Self {
+        Self {
+            width,
+            height,
+            format,
+        }
+    }
+}
+
+#[wasm_bindgen]
+#[derive(Clone, Debug)]
 pub struct Mince {
-    inner: Vec<u8>,
-    format: ImageFormat,
+    inner: Box<DynamicImage>,
+    meta: Metadata,
 }
 
 #[wasm_bindgen]
 impl Mince {
+    /// Creates a new `Mince` instance from a Image RS `DynamicImage`
+    fn new(dynamic_image: DynamicImage) -> Mince {
+        let meta = Metadata::default();
+
+        Self {
+            inner: Box::new(dynamic_image),
+            meta,
+        }
+    }
+
+    /// Reads a browser file into a `Mince` instance
     pub async fn from_file(file: File) -> Mince {
         let array_buffer = JsFuture::from(file.array_buffer()).await.unwrap();
         let uint8_array = Uint8Array::new(&array_buffer);
@@ -25,11 +78,26 @@ impl Mince {
         let cursor = Cursor::new(inner.clone());
         let reader = ImageReader::new(cursor).with_guessed_format().unwrap();
         let format = reader.format().unwrap();
+        let image = reader.decode().unwrap();
+        let meta = Metadata::new(image.width(), image.height(), format.into());
 
-        Self { inner, format }
+        Self {
+            inner: Box::new(image),
+            meta,
+        }
     }
 
-    pub fn format(&self) -> String {
-        format!("{:?}", self.format)
+    pub fn meta(&self) -> Metadata {
+        self.meta
+    }
+
+    /// Resizes the image and returns a new instance of `Mince` containing it
+    pub fn resize(&self, width: u32, height: u32) -> Self {
+        use image::imageops;
+
+        let buf = imageops::resize(self.inner.as_ref(), width, height, FilterType::Lanczos3);
+        let dynamic_image = DynamicImage::ImageRgba8(buf);
+
+        Mince::new(dynamic_image)
     }
 }
